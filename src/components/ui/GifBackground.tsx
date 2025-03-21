958c5c364c6df228394506aa76c52831ddc04245
GioWashel <giowashel@gmail.com>
2025-03-20 21:27:25 -0400

    update 40 to 45

--- src/components/ui/GifBackground.tsx
+++ src/components/ui/GifBackground.tsx
@@ -63,12 +63,13 @@ const GifBackground = React.memo(() => {
         const backgroundGifs = useRef<string[]>(
             allGifs
               .sort(() => Math.random() - 0.5) // Shuffle the array
-              .slice(0, 40) // Take the first 40 GIFs
+              .slice(0, 45) // Take the first 40 GIFs
+      
           );
 
   return (
     <div className="gif-background">
-      {backgroundGifs.current.map((gif, index) => (
+      {allGifs.map((gif, index) => (
         <img
           key={index}
           src={gif}
@@ -83,7 +84,6 @@ const GifBackground = React.memo(() => {
             height: 'auto',
             zIndex: Math.random() * -1,
             borderRadius: 10,
-
           }}
         />
       ))}
